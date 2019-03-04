ddf698a7ac80ef5f7869b9d7274d806bd043d66d
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-04 19:36:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
 # your code goes here
+def begins_with_r(array)
+  array.each do |string|
+    if string.split(//).first != "r"
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  array.select { |e| e.include? ?a  }
+end
+
+def first_wa(array)
+  array.select { |e| e.slice(0,2) == "wa" }.shift
+end
+
+def remove_non_strings(array)
+  array.select { |e| e.is_a? String}
+end
+
+def count_elements(array)
+  count = []
+  array.uniq.each do |e|
+    element = e
+    element[:count] = array.count(e)
+    count << element
+  end
+  count
+end
+
+def merge_data(keys, data)
+  keys.concat(data)
+end
